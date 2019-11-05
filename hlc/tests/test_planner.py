4c993a619c4de7fd4ba491a72e4ac070c1bed9e8
Selma Illig <selma.illig@code.berlin>
2019-11-05 16:23:50 +0100

    Save obstacles in list

--- hlc/tests/test_planner.py
+++ hlc/tests/test_planner.py
@@ -1,7 +1,5 @@
 from hlc.planner import planner
 import numpy as np
-import sys
-sys.path.append('..')
 
 
 def test_empty_map_6x6():
@@ -32,6 +30,7 @@ def test_empty_map_6x6():
 def test_one_obstacle_6x6():
     keys = []
     pos = (0, 0)
+    obstacles = [(1,2)]
 
     # create all keys aka all position in the grid
     for i in range(6):
@@ -43,8 +42,7 @@ def test_one_obstacle_6x6():
     all_positions = dict.fromkeys(keys, False)
     all_positions[(0, 0)] = True
 
-    plan_output = planner.plan(
-        (6, 6), np.array([[1, 2]]), np.array([]), (0, 0))
+    plan_output = plan((6, 6), np.array([obstacles]), np.array([]), (0, 0))
 
     # if position visited change value to True
     for action in plan_output:
@@ -54,8 +52,8 @@ def test_one_obstacle_6x6():
     # pos_blocked needs to be False, all other pos True --> robot explored the
     # whole map with one obstacle
     for key, value in all_positions.items():
-        if key == (
-                1, 2):  # pos with obsticle, value needs to be false = pos not visited
+            # pos with obsticle, value needs to be false = pos not visited
+        if key in obstacles:
             assert value != True
         else:  # all other pos must be visited
             assert value == True
@@ -64,7 +62,7 @@ def test_one_obstacle_6x6():
 def test_multiple_obsticle_6x6():
     keys = []
     pos = (0, 0)
-
+    obstacles = [(0, 2), (1, 5), (3, 4), (5, 2)]
     # create all keys aka all position in the grid
     for i in range(6):
         for j in range(6):
@@ -75,8 +73,8 @@ def test_multiple_obsticle_6x6():
     all_positions = dict.fromkeys(keys, False)
     all_positions[(0, 0)] = True
 
-    plan_output = planner.plan((6, 6), np.array(
-        [[0, 2], [1, 5], [3, 4], [5, 2]]), np.array([]), (0, 0))
+    plan_output = plan((6, 6), np.array(
+        [obstacles]), np.array([]), (0, 0))
 
     # if position visited change value to True
     for action in plan_output:
@@ -86,23 +84,15 @@ def test_multiple_obsticle_6x6():
     # pos_blocked needs to be False, all other pos True --> robot explored the
     # whole map with multiple obstacles
     for key, value in all_positions.items():
-        if key == (
-                0, 2):  # pos with obsticle, value needs to be False = pos not visited
+        # pos with obsticle, value needs to be False = pos not visited
+        if key in obstacles:
             assert value != True
-        elif key == (1, 5):
-            assert value != True
-        elif key == (3, 4):
-            assert value != True
-        elif key == (5, 2):
-            assert value != True
-        else:  # all other pos must be visited
-            assert value == True
 
 
 def test_dead_end_6x6():
     keys = []
     pos = (0, 0)
-
+    obstacles = [(2, 1), (2, 2), (2, 3), (2, 4), (2, 5)]
     # create all keys aka all position in the grid
     for i in range(6):
         for j in range(6):
@@ -115,8 +105,7 @@ def test_dead_end_6x6():
 
     # only open pos at [2,0], robot still needs to explore all (0,x) and (1,x)
     # and then walk backwards to explore rest of map
-    plan_output = planner.plan((6, 6), np.array(
-        [[2, 1], [2, 2], [2, 3], [2, 4], [2, 5]]), np.array([]), (0, 0))
+    plan_output = plan((6, 6), np.array([obstacles]), np.array([]), (0, 0))
 
     # if position visited change value to True
     for action in plan_output:
@@ -126,19 +115,9 @@ def test_dead_end_6x6():
     # pos_blocked needs to be False, all other pos True --> robot explored the
     # whole map with one dead end
     for key, value in all_positions.items():
-        if key == (
-                1, 1):  # pos with obsticle, value needs to be false = pos not visited
+        # pos with obsticle, value needs to be false = pos not visited
+        if key in obstacles:
             assert value != True
-        elif key == (1, 2):
-            assert value != True
-        elif key == (1, 3):
-            assert value != True
-        elif key == (1, 4):
-            assert value != True
-        elif key == (1, 5):
-            assert value != True
-        else:  # all other pos must be visited
-            assert value == True
 
 
 # missing: start position (0,0) is blocked --> new start position or
