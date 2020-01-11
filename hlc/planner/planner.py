d22d276b843e610388281aa7e4a08a60c814f50d
animuslupus <16310725+Animuslupus@users.noreply.github.com>
2020-01-11 14:08:30 +0100

    Clarify switching layers

--- hlc/planner/planner.py
+++ hlc/planner/planner.py
@@ -8,6 +8,11 @@ def apply_actions(actions, position, plan):
         position.update(a)
 
 
+def update_position(pose, new_actions):
+    for action in new_actions:
+        pose.update(action)
+
+
 def plan(grid_dimension, start_pose=Pose(0, 0, 0), layer=0,):
     robot_pose = start_pose.copy()
     plan = []
@@ -20,9 +25,10 @@ def plan(grid_dimension, start_pose=Pose(0, 0, 0), layer=0,):
         apply_actions([HLAction.MOVE_FORWARD], robot_pose, plan)
 
         if robot_pose.get_position() == final_layer_position:
-            apply_actions(
-                [HLAction.TURN_RIGHT, HLAction.MOVE_FORWARD], robot_pose, plan)
-            final_layer_position = robot_pose.copy().add_tuple((1, 0))
+            switch_layer_actions = [HLAction.TURN_RIGHT, HLAction.MOVE_FORWARD]
+            plan.extend(switch_layer_actions)
+            update_position(robot_pose, switch_layer_actions)
+            final_layer_position = get_new_final_layer_position(robot_pose)
             layer += 1
         elif robot_pose.x == layer and robot_pose.y == grid_dimension[1] - layer:
             apply_actions([HLAction.TURN_RIGHT], robot_pose, plan)
@@ -41,3 +47,7 @@ def plan(grid_dimension, start_pose=Pose(0, 0, 0), layer=0,):
                 apply_actions([HLAction.MOVE_FORWARD], robot_pose, plan)
 
     return plan
+
+
+def get_new_final_layer_position(robot_pose):
+    return robot_pose.copy().add_tuple((1, 0))
