d30740c582e97f29014ee075147cfe7d313c815d
animuslupus <16310725+Animuslupus@users.noreply.github.com>
2020-01-11 14:54:18 +0100

    Extract layer-corners into layered Map class

--- hlc/planner/planner.py
+++ hlc/planner/planner.py
@@ -2,15 +2,41 @@
 from hlc.planner.helper import Position, Pose, HLAction
 
 
-def get_layer_corners(map_dimensions, layer_index):
-    left_bottom_corner = (layer_index, layer_index)
-    left_upper_corner = (layer_index, map_dimensions[1] - layer_index)
-    right_upper_corner = (
-        map_dimensions[0] - layer_index,
-        map_dimensions[1] - layer_index)
-    right_bottom_corner = (map_dimensions[0]-layer_index, layer_index)
+class Map2D():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
-    return left_bottom_corner, left_upper_corner, right_upper_corner, right_bottom_corner
+
+class Layered2DMap(Map2D):
+
+    def __init__(self, width, height, layer_index):
+        self.width = width
+        self.height = height
+        self.layer_index = layer_index
+        self._update_all_layer_corners()
+
+    def _get_left_bottom_layer_corner(self):
+        return (self.layer_index, self.layer_index)
+
+    def _get_left_upper_layer_corner(self):
+        return (self.layer_index, self.height - self.layer_index)
+
+    def _get_right_upper_layer_corner(self):
+        return (self.width - self.layer_index, self.height - self.layer_index)
+
+    def _get_right_bottom_layer_corner(self):
+        return (self.width-self.layer_index, self.layer_index)
+
+    def _update_all_layer_corners(self):
+        self.left_bottom_layer_corner = self._get_left_bottom_layer_corner()
+        self.left_upper_layer_corner = self._get_left_upper_layer_corner()
+        self.right_upper_layer_corner = self._get_right_upper_layer_corner()
+        self.right_bottom_layer_corner = self._get_right_bottom_layer_corner()
+
+    def switchLayer(self, layer_index):
+        self.layer_index = layer_index
+        self._update_all_layer_corners()
 
 
 def apply_actions(actions, position, plan):
@@ -29,8 +55,7 @@ def plan(grid_dimension, start_pose=Pose(0, 0, 0), layer_index=0,):
     plan = []
     final_layer_position = start_pose.add_tuple((1, 0))
 
-    corners = get_layer_corners(grid_dimension, layer_index)
-    _, left_upper_corner, right_upper_corner, right_bottom_corner = corners
+    layerMap = Layered2DMap(grid_dimension[0], grid_dimension[1], layer_index)
 
     max_layer_index = max(grid_dimension) // 2
 
@@ -43,15 +68,13 @@ def plan(grid_dimension, start_pose=Pose(0, 0, 0), layer_index=0,):
             plan.extend(switch_layer_actions)
             update_position(robot_pose, switch_layer_actions)
             final_layer_position = get_new_final_layer_position(robot_pose)
-
             layer_index += 1
-            corners = get_layer_corners(grid_dimension, layer_index)
-            _, left_upper_corner, right_upper_corner, right_bottom_corner = corners
-        elif (robot_pose.x, robot_pose.y) == left_upper_corner:
+            layerMap.switchLayer(layer_index)
+        elif (robot_pose.x, robot_pose.y) == layerMap.left_upper_layer_corner:
             apply_actions([HLAction.TURN_RIGHT], robot_pose, plan)
-        elif (robot_pose.x, robot_pose.y) == right_upper_corner:
+        elif (robot_pose.x, robot_pose.y) == layerMap.right_upper_layer_corner:
             apply_actions([HLAction.TURN_RIGHT], robot_pose, plan)
-        elif (robot_pose.x, robot_pose.y) == right_bottom_corner:
+        elif (robot_pose.x, robot_pose.y) == layerMap.right_bottom_layer_corner:
             apply_actions([HLAction.TURN_RIGHT], robot_pose, plan)
 
     if grid_dimension[0] != grid_dimension[1] and layer_index == max_layer_index:
