34b9a1ffcd2ff3ad1f4681110f11060deee1b27f
animuslupus <16310725+Animuslupus@users.noreply.github.com>
2020-01-12 17:59:04 +0100

    Add Layer & Corner class

--- hlc/planner/planner.py
+++ hlc/planner/planner.py
@@ -1,8 +1,9 @@
 # from hlc.config.constants import *
-from hlc.planner.helper import Position, Pose, HLAction
+from hlc.planner.helper import Pose, HLAction
 from typing import Tuple, List
+from math import sqrt
 
-Corner = Tuple[int, int]
+Position = Tuple[int, int]
 
 
 class Map2D():
@@ -19,16 +20,16 @@ class Layered2DMap(Map2D):
         self.layer_index = layer_index
         self._update_all_layer_corners()
 
-    def _get_left_bottom_layer_corner(self) -> Corner:
+    def _get_left_bottom_layer_corner(self) -> Position:
         return (self.layer_index, self.layer_index)
 
-    def _get_left_upper_layer_corner(self) -> Corner:
+    def _get_left_upper_layer_corner(self) -> Position:
         return (self.layer_index, self.height - self.layer_index - 1)
 
-    def _get_right_upper_layer_corner(self) -> Corner:
+    def _get_right_upper_layer_corner(self) -> Position:
         return (self.width - self.layer_index - 1, self.height - self.layer_index - 1)
 
-    def _get_right_bottom_layer_corner(self) -> Corner:
+    def _get_right_bottom_layer_corner(self) -> Position:
         return (self.width - self.layer_index - 1, self.layer_index)
 
     def _update_all_layer_corners(self):
@@ -40,6 +41,59 @@ class Layered2DMap(Map2D):
     def switchLayer(self, layer_index: int):
         self.layer_index = layer_index
         self._update_all_layer_corners()
+
+    def generate_rectangle_layer(self):
+        min_width = self.layer_index
+        min_height = self.layer_index
+        max_width = self.width - self.layer_index - 1
+        max_height = self.height - self.layer_index - 1
+
+        corner_positions = [
+            (min_width, min_height),
+            (min_width, max_height),
+            (max_width, max_height),
+            (max_width, min_height)
+        ]
+        return Layer(corner_positions)
+
+
+class Corner():
+    def __init__(self, position: Position):
+        self.position = position
+        self.next_corner: Corner
+        self.previous_corner: Corner
+
+
+class Layer():
+    def __init__(self, corner_positions: List[Position]):
+        self.corners: List[Corner]
+        self.corners = []
+
+        for position in corner_positions:
+            self.corners.append(Corner(position))
+
+        self.corners[0].previous_corner = self.corners[-1]
+        self.corners[-1].next_corner = self.corners[0]
+        for i in range(1, len(self.corners) - 1):
+            self.corners[i].previous_corner = self.corners[i-1]
+            self.corners[i].next_corner = self.corners[i+1]
+
+    def get_closest_corner(self, position: Position):
+        min_distance = float("inf")
+        closest_corner = None
+        for c in self.corners:
+            distance = get_distance(c.position, position)
+            if distance < min_distance:
+                closest_corner = c
+                min_distance = distance
+        return closest_corner
+
+
+def get_distance(point1: Position, point2: Position):
+    x_difference = point1[0] - point2[0]
+    y_difference = point1[1] - point2[1]
+    total_distance = sqrt(x_difference**2 + y_difference**2)
+    return total_distance
 
 
 def apply_actions(actions: List[HLAction], robot_pose: Pose, plan: List[HLAction]):
@@ -64,8 +118,10 @@ def plan(grid_width: int, grid_height: int, start_pose=Pose(0, 0, 0), layer_inde
 
     while layer_map.layer_index < max_layer_index:
 
+        layer = layer_map.generate_rectangle_layer()
+
         actions = progress_through_layer(
-            layer_map, robot_pose, final_layer_position)
+            layer, robot_pose, final_layer_position)
         plan.extend(actions)
 
         switch_layer_actions = [HLAction.TURN_RIGHT, HLAction.MOVE_FORWARD]
@@ -85,20 +141,20 @@ def plan(grid_width: int, grid_height: int, start_pose=Pose(0, 0, 0), layer_inde
     return plan
 
 
-def get_new_final_layer_position(robot_pose: Pose) -> Tuple[int, int]:
+def get_new_final_layer_position(robot_pose: Pose) -> Position:
     return robot_pose.copy().add_tuple((1, 0))
 
 
-def progress_through_layer(layer_map: Layered2DMap, robot_pose: Pose, final_layer_position: Tuple[int, int]) -> List[HLAction]:
+def progress_through_layer(layer: Layer, robot_pose: Pose, final_layer_position: Position) -> List[HLAction]:
     progress = []
     while robot_pose.get_position() != final_layer_position:
         apply_actions([HLAction.MOVE_FORWARD], robot_pose, progress)
 
-        if layer_map.left_upper_layer_corner == robot_pose.get_position():
+        if layer.corners[1].position == robot_pose.get_position():
             apply_actions([HLAction.TURN_RIGHT], robot_pose, progress)
-        elif layer_map.right_upper_layer_corner == robot_pose.get_position():
+        elif layer.corners[2].position == robot_pose.get_position():
             apply_actions([HLAction.TURN_RIGHT], robot_pose, progress)
-        elif layer_map.right_bottom_layer_corner == robot_pose.get_position():
+        elif layer.corners[3].position == robot_pose.get_position():
             apply_actions([HLAction.TURN_RIGHT], robot_pose, progress)
 
     return progress
