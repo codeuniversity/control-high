32cef2f987cdcedf2e518ab649ffd771c0b05447
animuslupus <16310725+Animuslupus@users.noreply.github.com>
2020-01-13 11:25:22 +0100

    Add timeout to tests

--- hlc/tests/helper.py
+++ hlc/tests/helper.py
@@ -28,11 +28,30 @@ def navigate_grid(plan: List[HLAction], grid: Map2D, position: Pose):
         grid[position.get_position()] = True
 
 
+def process_wrapper(function, args, result_queue):
+    result = function(*args)
+    result_queue.put(result)
+
+
+def generate_plan_with_timeout(timeout, plan_args):
+    result_queue = Queue()
+    planing_process = Process(target=process_wrapper,
+                              args=(plan, plan_args, result_queue))
+    planing_process.start()
+    planing_process.join(timeout)
+
+    if planing_process.is_alive():
+        planing_process.terminate()
+        raise TimeoutError("The Planning function took too long to finish")
+    return result_queue.get()
+
+
 def create_solution_grid(grid_width: int, grid_height: int, start_pose: Pose, obstacle_positions=[]):
     grid = Map2D(grid_width, grid_height, obstacle_positions)
     grid[start_pose.get_position()] = True
 
-    plan_output = plan(grid_width, grid_height, start_pose.copy())
+    plan_output = generate_plan_with_timeout(
+        2, (grid_width, grid_height, start_pose.copy()))
     navigate_grid(plan_output, grid, start_pose)
 
     return grid
