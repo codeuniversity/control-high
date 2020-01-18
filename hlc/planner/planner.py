c4a3552d4d789506ec6a7df868cab3615104033b
animuslupus <16310725+Animuslupus@users.noreply.github.com>
2020-01-18 13:10:18 +0100

    Extract agent related function into class

--- hlc/planner/planner.py
+++ hlc/planner/planner.py
@@ -7,16 +7,16 @@ import decimal
 
 
 def generate_plan(target_map: Layered2DMap, start_pose=Pose(0, 0, 0)) -> List[HLAction]:
+    robot = Agent(start_pose)
 
     max_layer_index = min(target_map.width, target_map.height) / 2
 
-    robot_pose = start_pose.copy()
     plan = []
     while target_map.layer_index < max_layer_index:
 
         layer = target_map.generate_rectangle_layer()
 
-        actions = progress_through_layer(layer, robot_pose)
+        actions = robot.progress_through_layer(layer)
         plan.extend(actions)
 
         target_map.switchLayer(target_map.layer_index + 1)
@@ -24,60 +24,62 @@ def generate_plan(target_map: Layered2DMap, start_pose=Pose(0, 0, 0)) -> List[HL
     return plan
 
 
-def align_orientation(vector: Vector, robot_pose: Pose):
-    y_axis = robot_pose.orientation
-    x_axis = robot_pose.orientation.rotate(90)
+class Agent():
+    def __init__(self, start_pose: Pose):
+        self.pose = start_pose
 
-    angle_y_axis = vector.get_angle_to(y_axis)
-    angle_x_axis = vector.get_angle_to(x_axis)
-    if angle_x_axis <= 90:
-        rotation_direction = 1
-    else:
-        rotation_direction = -1
+    def progress_through_layer(self, rectangle_layer: Layer) -> List[HLAction]:
+        progress = []
+        current_corner = rectangle_layer.get_closest_corner(self.pose.position)
+        for _ in range(len(rectangle_layer.corners) + 1):
+            actions = self.move_to(current_corner.position)
+            progress.extend(actions)
+            current_corner = current_corner.next_corner
+        return progress
 
-    actions = []
-    for _ in range(0, int(angle_y_axis), 90):
-        a = HLAction((0, 0, rotation_direction * 90))
-        robot_pose.apply_action(a)
-        actions.append(a)
+    def move_to(self, position: Vector) -> List[HLAction]:
+        x_difference = int(position.get_x() - self.pose.position.get_x())
+        y_difference = int(position.get_y() - self.pose.position.get_y())
 
-    return actions
+        actions = []
 
+        if x_difference != 0:
+            actions.extend(
+                self.align_orientation(Vector(x_difference, 0))
+            )
 
-def move_to(position: Vector, robot_pose: Pose) -> List[HLAction]:
-    x_difference = int(position.get_x() - robot_pose.position.get_x())
-    y_difference = int(position.get_y() - robot_pose.position.get_y())
+            for _ in range(abs(x_difference)):
+                a = HLAction.MOVE_FORWARD
+                self.pose.apply_action(a)
+                actions.append(a)
 
-    actions = []
+        if y_difference != 0:
+            actions.extend(
+                self.align_orientation(Vector(0, y_difference))
+            )
 
-    if x_difference != 0:
-        actions.extend(
-            align_orientation(Vector(x_difference, 0), robot_pose)
-        )
+            for _ in range(abs(y_difference)):
+                a = HLAction.MOVE_FORWARD
+                self.pose.apply_action(a)
+                actions.append(a)
 
-        for _ in range(abs(x_difference)):
-            a = HLAction.MOVE_FORWARD
-            robot_pose.apply_action(a)
+        return actions
+
+    def align_orientation(self, vector: Vector):
+        y_axis = self.pose.orientation
+        x_axis = self.pose.orientation.rotate(90)
+
+        angle_y_axis = vector.get_angle_to(y_axis)
+        angle_x_axis = vector.get_angle_to(x_axis)
+        if angle_x_axis <= 90:
+            rotation_direction = 1
+        else:
+            rotation_direction = -1
+
+        actions = []
+        for _ in range(0, int(angle_y_axis), 90):
+            a = HLAction((0, 0, rotation_direction * 90))
+            self.pose.apply_action(a)
             actions.append(a)
 
-    if y_difference != 0:
-        actions.extend(
-            align_orientation(Vector(0, y_difference), robot_pose)
-        )
-
-        for _ in range(abs(y_difference)):
-            a = HLAction.MOVE_FORWARD
-            robot_pose.apply_action(a)
-            actions.append(a)
-
-    return actions
-
-
-def progress_through_layer(layer: Layer, robot_pose: Pose) -> List[HLAction]:
-    progress = []
-    current_corner = layer.get_closest_corner(robot_pose.position)
-    for _ in range(len(layer.corners) + 1):
-        actions = move_to(current_corner.position, robot_pose)
-        progress.extend(actions)
-        current_corner = current_corner.next_corner
-    return progress
+        return actions
